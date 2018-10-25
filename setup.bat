1968c24fa9d620aa59a305de96dd7c88a246b519
Milad Mehdizade <milad.mehdizade1394@gmail.com>
2018-10-25 20:28:39 +0000

    windows users quick setup script added.

---
+++ setup.bat
@@ -0,0 +1,17 @@
+@echo off
+prompt #
+
+echo. & echo installing docx module & echo.
+python.exe -m pip install python-docx & echo.
+
+
+
+echo. & echo installing feedparsed module & echo.
+python.exe -m pip install feedparser & echo.
+
+echo @echo off > run.bat
+echo python.exe main.py >> run.bat
+
+
+echo Done. & echo. & echo Now execute run.bat
+prompt
