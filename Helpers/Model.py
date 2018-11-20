69352ddf60b7d3591ced903b30686d0b5277a702
Milad Mehdizade <44354927+MehdizadeMilad@users.noreply.github.com>
2018-11-20 16:09:30 +0330

    added Assets monitor.
    Added assets to check if any important asset is listed in the latest news.

--- Helpers/Model.py
+++ Helpers/Model.py
@@ -9,23 +9,35 @@ class news():
         self.priority = None
 
 listOfnewsSources = [
-    'https://exploit.kitploit.com/feeds/posts/default?alt=rss',
-    'https://www.secnews24.com/feed/',
-    'https://www.us-cert.gov/ncas/current-activity.xml',
-    'https://www.us-cert.gov/ncas/alerts.xml',
-    'https://ics-cert.us-cert.gov/alerts/alerts.xml',
-    'https://ics-cert.us-cert.gov/advisories/advisories.xml'
+    'http://securityaffairs.co/wordpress/feed ', 
+    'https://access.redhat.com/blogs/766093/feed', 
+    'https://cxsecurity.com/wlb/rss/all/', 
+    'https://exploit.kitploit.com/feeds/posts/default?alt=rss', 
+    'https://ics-cert.us-cert.gov/advisories/advisories.xml', 
+    'https://ics-cert.us-cert.gov/alerts/alerts.xml', 
+    'https://rss.packetstormsecurity.com/files/', 
+    'https://searchsecurity.techtarget.com/rss/Security-Wire-Daily-News.xml', 
+    'https://securelist.com/feed/', 
+    'https://tools.cisco.com/security/center/psirtrss20/AlertRSS.xml', 
+    'https://tools.cisco.com/security/center/psirtrss20/CiscoSecurityAdvisory.xml', 
+    'https://twitrss.me/twitter_user_to_rss/?user=CVEnew', 
+    'https://www.novell.com/newsfeeds/rss/patches/security_notifications-daily.xml', 
+    'https://www.oracle.com/ocom/groups/public/@otn/documents/webcontent/rss-otn-sec.xml', 
+    'https://www.secnews24.com/feed/', 
+    'https://www.us-cert.gov/ncas/alerts.xml', 
+    'https://www.us-cert.gov/ncas/current-activity.xml', 
+    'https://www.vulnerability-lab.com/rss/rss.php',
     'https://securelist.com/feed/',
-    'https://tools.cisco.com/security/center/psirtrss20/AlertRSS.xml',
-    'https://tools.cisco.com/security/center/psirtrss20/CiscoSecurityAdvisory.xml',
-    'https://www.novell.com/newsfeeds/rss/patches/security_notifications-daily.xml',
-    'https://access.redhat.com/blogs/766093/feed',
-    'https://www.oracle.com/ocom/groups/public/@otn/documents/webcontent/rss-otn-sec.xml'
+    'https://blog.yoroi.company/feed/',
+    'http://feeds.feedburner.com/eset/blog/',
+    'https://blog.malwarebytes.com/feed/',
+    'https://www.certcc.ir/news/rss'
  ]
-
-    #'https://www.certcc.ir/news/rss',
 
 #Global variables
 localNewsRepository = []
 localRedundantNewsRepository = []
+assetRelatedNewsItems = []
 baselineDate = 1
+assets=[ 'MariaDB', 'mysql', ' PHP ', ' ASP ', 'Ansible', 'Windows', 'ssh', 'Snmp', 'imaps',
+        'pop3s', 'smtps']
