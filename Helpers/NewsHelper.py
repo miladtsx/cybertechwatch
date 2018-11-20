c581b2ff07a54892e766bf34ea6ded4e9bc3cf9f
Milad Mehdizade <44354927+MehdizadeMilad@users.noreply.github.com>
2018-11-20 16:10:26 +0330

    added Asset monitoring.
    Added assets to check if any important asset is listed in latest news and…

--- Helpers/NewsHelper.py
+++ Helpers/NewsHelper.py
@@ -1,10 +1,19 @@
-from Model import listOfnewsSources, news, localNewsRepository, localRedundantNewsRepository
+from Model import listOfnewsSources, news, localNewsRepository, localRedundantNewsRepository, assetRelatedNewsItems, assets
 from HelperMethods import doesItMatch, getRSSContent, freshNews
 from TaskHelper import taskRun
+import traceback
 
 def updateNewsDB():
 
     taskRun(getNewsOnline, listOfnewsSources, len(listOfnewsSources))
+    
+    for redunItem in localRedundantNewsRepository:
+        print '[Match] ' + redunItem.title[:100]
+    
+    if (len(assetRelatedNewsItems) > 0):
+            print '*' * 10 + 'ASSETS' + '*' * 10 
+            for assetItem in assetRelatedNewsItems:
+                print '[ ' + assetItem.title[:100] + ' ]'
 
 def getNewsOnline(url):
     try:
@@ -18,6 +27,7 @@ def getNewsOnline(url):
             print '\r' + '[' + str(webContent.status) + '] ' + url
 
         for newsItem in newsGot:
+
             if(not freshNews(newsItem.published)):
                 continue
             try:
@@ -31,6 +41,14 @@ def getNewsOnline(url):
                 localNewsRepository.append(newsItemReceived)
                 continue
 
+            try:
+
+                for asset in assets:
+                    if(asset.lower() in newsItem.title.lower()):
+                        assetRelatedNewsItems.append(newsItem)
+            except:
+                raise TypeError("Checking the Assets Failed!")
+
             matchFound = False
             for localRep in localNewsRepository:
                 if (doesItMatch(newsItemReceived.title, localRep.title)):
@@ -43,5 +61,5 @@ def getNewsOnline(url):
                 continue
             else:
                 localNewsRepository.append(newsItemReceived)
-    except Exception, e:
-        print '[Err] ' + url + '\n' + str(e)
+    except Exception:
+        print '[Err] ' + url + '\n' + traceback.print_exc()
