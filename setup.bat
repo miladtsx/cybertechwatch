e2469a91f0c8607a6512cb7fdbd4420e4d0aaf1a
Milad Mehdizade <44354927+MehdizadeMilad@users.noreply.github.com>
2018-11-24 08:27:55 +0330

    Update setup.bat
    dateutil module added.

--- setup.bat
+++ setup.bat
@@ -5,9 +5,11 @@ echo. & echo installing docx module & echo.
 python.exe -m pip install python-docx & echo.
 
 
-
 echo. & echo installing feedparsed module & echo.
 python.exe -m pip install feedparser & echo.
+
+echo. & echo installing dateutil module & echo.
+python.exe -m pip install python-dateutil & echo.
 
 echo @echo off > run.bat
 echo python.exe main.py >> run.bat
