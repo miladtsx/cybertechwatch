ebb64cbc95ae53006722a76795642e44bd8987c2
Milad Mehdizade <44354927+MehdizadeMilad@users.noreply.github.com>
2019-04-07 10:40:22 +0430

    Adding fg class for Coloring terminal output.

--- Helpers/Model.py
+++ Helpers/Model.py
@@ -20,7 +20,6 @@ listOfnewsSources = [
     'https://securelist.com/feed/', 
     'https://tools.cisco.com/security/center/psirtrss20/AlertRSS.xml', 
     'https://tools.cisco.com/security/center/psirtrss20/CiscoSecurityAdvisory.xml', 
-    'https://twitrss.me/twitter_user_to_rss/?user=CVEnew', 
     'https://www.novell.com/newsfeeds/rss/patches/security_notifications-daily.xml', 
     'https://www.oracle.com/ocom/groups/public/@otn/documents/webcontent/rss-otn-sec.xml', 
     'https://www.secnews24.com/feed/', 
@@ -31,7 +30,10 @@ listOfnewsSources = [
     'https://blog.yoroi.company/feed/',
     'http://feeds.feedburner.com/eset/blog/',
     'https://blog.malwarebytes.com/feed/',
-    'https://www.certcc.ir/news/rss'
+    'https://linuxsecurity.com/advisories/debian?format=feed&type=rss',
+    'https://linuxsecurity.com/advisories/deblts?format=feed&type=rss',
+    'https://linuxsecurity.com/advisories/centos?format=feed&type=rss',
+    'https://linuxsecurity.com/advisories/red-hat?format=feed&type=rss',
  ]
 
 #Global variables
@@ -39,5 +41,18 @@ localNewsRepository = []
 localRedundantNewsRepository = []
 assetRelatedNewsItems = []
 baselineDate = 1
-assets=[ 'MariaDB', 'mysql', ' PHP ', ' ASP ', 'Ansible', 'Windows', 'ssh', 'Snmp', 'imaps',
+assets=[ 'MariaDB', 'mysql', ' ASP ', 'Ansible', 'Windows', 'ssh', 'Snmp', 'imaps',
         'pop3s', 'smtps']
+
+class fg:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    RESET = '\033[39m'
+
+    
