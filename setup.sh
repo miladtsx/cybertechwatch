377bcc97ec2684a34baa4a9dd677140006485c4f
Milad Mehdizade <44354927+MehdizadeMilad@users.noreply.github.com>
2019-04-07 10:36:54 +0430

    using python's virtualenv to setup.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-echo "Trying to install requirements ...\n"
-
+printf "Installing requirements ...\n"
 printf "#!/bin/bash\n python ./main.py" > run.sh
-
 printf "python-docx>=0.8.7\n feedparser>=5.2.1\n python-dateutil>=2.7.3\n" > requirements.txt
 
-printf "\nInstalling requirements ...\n"
-python -m pip install -r requirements.txt --user
-rm requirements.txt
+python -m pip install virtualenv --user
+python -m virtualenv cyberNewsPyEnv
+source cyberNewsPyEnv/bin/activate
 
-printf "Done.\n Execute chmod +x run.sh then execute ./run.sh\n"
+python -m pip install -r requirements.txt
+rm requirements.txt
+chmod +x run.sh
+./run.sh
+
+printf "Done.\nFor later uses just do: ./run.sh\n"
